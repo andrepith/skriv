cafe7dbabd850a0997785b49f23300ee307f1047
Andre Adikara <andrepith@yahoo.co.uk>
2020-04-06 10:36:14 +0700

    Create card component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/lib/integration/react";
 import { store, persistor } from "./store";
+import Container from "containers";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        <PersistGate persistor={persistor}></PersistGate>
+        <PersistGate persistor={persistor}>
+          <Container />
+        </PersistGate>
       </Provider>
     </div>
   );
---
+++ src/containers/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Card } from "components";
+
+export default () => {
+  return (
+    <div>
+      <Card />
+    </div>
+  );
+};
